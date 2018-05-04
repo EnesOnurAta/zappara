9ed511b4573363c29164c76332bf3770e963725f
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-04 23:42:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,3 +25,44 @@ bot.on('message', async msg => {
     msg.react('🇸');
   }
 });
+
+/* SOHBET TEMIZLEME */
+bot.on('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'sil') {
+    if (msg.channel.type === 'dm') {
+      const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField(':warning: Uyarı :warning:', 'Bu komutu özel mesajlarda kullanamazsın.')
+    msg.author.sendEmbed(ozelmesajuyari); }
+      if (msg.channel.type !== 'dm') {
+        if (!msg.member.hasPermission("MANAGE_MESSAGES")) {
+          if (msg.author.id !== '274551537139712001') {
+            const mesajlariyonet = new Discord.RichEmbed()
+          .setColor(0xFF0000)
+          .setTimestamp()
+          .setAuthor(msg.author.username, msg.author.avatarURL)
+          .addField(':warning: Uyarı :warning:', 'Bu komutu kullanmak için `Mesajları Yönet` iznine sahip olmalısın.')
+          return msg.author.sendEmbed(mesajlariyonet);
+      }}
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100);
+      msg.channel.bulkDelete(100); //1000 mesaj gg
+      const sohbetsilindi = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('Eylem:', 'Sohbet silme')
+    .addField('Yetkili:', msg.author.username)
+    .addField('Sonuç:', `1000 Mesaj Silindi`)
+    return msg.channel.sendEmbed(sohbetsilindi);
+      console.log("Sohbet " + msg.member + " tarafından silindi!");
+      console.log("1000 mesaj gg oldu :)");
+}}});
