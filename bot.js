e01ff8b931de693892d298105970519325eb73b5
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-05 00:20:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,12 +11,47 @@ bot.on('ready', () => {
 });
 
 /* OYNUYOR */
-bot.on('ready', () => {
-  bot.user.setStatus("PLAYING"); 
-  bot.user.setActivity('Yardım Komutu: z!yardım', {
-    type: "PLAYING"
-  }); 
-})
+exports.run = (client, msg, args) => {
+    let statuses = {
+      "online": "online",
+      "on": "online",
+      "invisible": "invisible",
+      "offline": "invisible",
+      "off": "invisible",
+      "invis": "invisible",
+      "i": "invisible",
+      "dnd": "dnd",
+      "idle": "idle"
+    };
+    if(!args[0]) return msg.edit(`Zappara | www.zappara.cf`).then(setTimeout(msg.delete.bind(msg), 1000));
+    let status = statuses[args[0].toLowerCase()];
+    if(!status) {
+      return msg.edit(`Yardım Komutu ${status} z!yardım`).then(setTimeout(msg.delete.bind(msg), 1000));
+    }
+    if(status === "on") status = "online";
+    if(status === "off") status = "invisible";
+    if(status === "i") status = "invisible";
+    if(status === "offline") status = "invisible";
+    client.user.setStatus(status)
+    .then(u=> {
+      msg.edit(`Status changed to ${status}`).then(setTimeout(msg.delete.bind(msg), 1000));
+    }).catch(e=> {
+      msg.edit(`Error while changing status to: ${status}\n\`\`\`${e}\`\`\``).then(setTimeout(msg.delete.bind(msg), 1000));
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["s"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'status',
+  description: 'Changes the Status of the bot/user',
+  usage: 'status [online/invisible/dnd/idle]'
+};
 
 /* SELAM VERME */
 bot.on('message', async msg => {
