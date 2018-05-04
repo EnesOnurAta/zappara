978fefb965716c9a7c76f342e1838904a206fb6f
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-04 23:29:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,3 +23,17 @@ bot.on('message', msg => {
   if (msg.content === prefix + 'ping') {
     msg.reply(`**${bot.ping}**ms gecikme`);
   }
+  
+  /* YAZDIRMA */
+bot.on('message', msg => {
+    if (msg.content.startsWith(prefix + "yaz")) {
+      if (msg.channel.type !== "dm"){
+      let mesaj = msg.content.substring(2 + 3);
+      msg.delete (msg.content == 'yaz' + mesaj)
+      let embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+         .setDescription(mesaj)
+  return msg.channel.send({embed})}
+  
+      }
+      });
