a788b24a7e63dfc01fed3a6aad28ba0c08d3372b
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-05 09:39:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,12 @@ bot.on('ready', () => {
 });
 
 /* OYNUYOR */
-
+bot.on('ready', () => {
+  bot.user.setStatus("PLAYING"); 
+  bot.user.setActivity('z!yardım | Zappara', {
+    type: "PLAYING"
+  }); 
+})
 
 /* SELAM VERME */
 bot.on('message', async msg => {
@@ -55,54 +60,7 @@ bot.on('message', msg => {
     .setTimestamp()
     .addField('Eylem:', 'Sohbet silme')
     .addField('Yetkili:', msg.author.username)
-    .addField('Sonuç:', `1000 Mesaj Silindi`)
+    .addField('Sonuç:', `1000 Mesaj Silindi :white_check_mark:`)
     return msg.channel.sendEmbed(sohbetsilindi);
       console.log("Sohbet " + msg.member + " tarafından silindi!");
-      console.log("1000 mesaj gg oldu :)");
 }}});
-
-/* KULLANICI BILGI */
-bot.on('message', message => { //Message Event | Listener
-
-    if (message.content.startsWith(prefix + 'profil')) {
-
-        const PROFIL = new Discord.MessageEmbed()
-
-            //All Fields are Optional Pick Any some
-
-            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
-            .setTitle('PROFIL')
-            .setURL('www.google.com') //Any Vaild Link
-            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
-            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
-            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
-
-            //All Feilds Are Just Examples pick Some & add as you like
-
-            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
-            .addField('AvatarURL', message.author.avatarURL({
-                format: 'png'
-            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
-            .addField('AvatarURL', message.author.avatarURL({
-                size: '2048'
-            }), true)
-            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Hesabı Açtığı Tarih', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
-            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
-            .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Son Mesajı', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('Son Mesaj ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Presence', message.author.presence) //The presence of this user
-            .addField('Durumu', message.author.presence.status) //The presence status of this user
-            .addField('Oynadığı Oyun', message.author.presence.activity.name) //The presence Game of this user
-            .addField('Etiket', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Kullanıcı Adı', message.author.username) //The username of the user || Ex:- Sai Chinna
-            .addField('Sunucudaki Kullanıcı Adı', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-
-            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
-            .setTimestamp() //The timestamp of this embed
-
-        message.channel.send(PROFIL);
-    }
-});
