cef2e7fcf0011ad3ed3451c0a399de23fbfcb962
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-08 19:23:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,3 +115,15 @@ bot.on('message', msg => {
   if (msg.content.toLowerCase().includes('Zappara')) msg.reply('Efendim Canım!');
   if (msg.content.toLowerCase().includes('Evlatlarım')) msg.reply('Babaam!');
 });
+
+bot.on('message', msg => {
+    if (msg.content === prefix + 'sunucu') {
+      const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setThumbnail(msg.author.avatarURL)
+        .addField("Sunucu Sahibi", msg.guild.owner)
+        .addField("Toplam Üye Sayısı", msg.guild.members.size)
+        .addField("Oluşturulma Tarihi", msg.guild.createdAt)
+      msg.channel.send(embed)
+    }
+  });
