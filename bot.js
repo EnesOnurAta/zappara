db1c2ae26eb4ebc69380e5b57e4cf774ed097b88
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-13 00:12:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,15 +88,15 @@ bot.on('message', msg => {
 }}});
 
 /* YAZ */
-bot.on('message', msg => {
+client.on('message', msg => {
     if (msg.content.startsWith(prefix + "yaz")) {
       if (msg.channel.type !== "dm"){
       let mesaj = msg.content.substring(2 + 3);
       msg.delete (msg.content == 'yaz' + mesaj)
-      let yazkomutembed = new Discord.RichEmbed()
+      let embed = new Discord.RichEmbed()
       .setColor("RANDOM")
          .setDescription(mesaj)
-  return msg.channel.send({yazkomutembed})}
+  return msg.channel.send({embed})}
   
       }
       });
