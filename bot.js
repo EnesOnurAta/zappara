4d479dc7367a5a2ac9384aa532c6d9e9d6bc31a4
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-13 01:11:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
   console.log('Giris Saglandi');
   console.log("Prefix: " + prefix);
   console.log("Bot ID'si:" + client.user.id);
-  console.log("Bot Isim: ${client.user.username}');
+  console.log('Bot Isim: ${client.user.username}');
 });
 
 /* OYNUYOR */
