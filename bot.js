676db168d4f9094a90a1fdf7ad94352c6d5cc3f3
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-13 00:08:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,10 +93,10 @@ bot.on('message', msg => {
       if (msg.channel.type !== "dm"){
       let mesaj = msg.content.substring(2 + 3);
       msg.delete (msg.content == 'yaz' + mesaj)
-      let embed = new Discord.RichEmbed()
+      let yazkomutembed = new Discord.RichEmbed()
       .setColor("RANDOM")
          .setDescription(mesaj)
-  return msg.channel.send({embed})}
+  return msg.channel.send({yazkomutembed})}
   
       }
       });
