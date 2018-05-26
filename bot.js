ce6629a26c780e64658c58232bf3ca36d9980500
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-26 23:28:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,67 +196,10 @@ client.on('message', message => {
             {            
                 client.user.setAvatar('https://cdn.discordapp.com/attachments/304630731345362946/450009096406433802/letter_1.png').catch(console.log);
             }
-           }});
+});
 
-/*
-client.on('message', message => {
-    if (message.content.startsWith(prefix + 'kullanıcıbilgi')) {
-        const KullanıcıBilgi = new Discord.MessageEmbed()
-        
-            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
-            .setTitle('Kullanıcı Bilgi')
-            .setURL('www.google.com') //Any Vaild Link
-            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
-            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
-            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
-            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
-            .addField('AvatarURL', message.author.avatarURL({
-                format: 'png'
-            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
-            .addField('AvatarURL', message.author.avatarURL({
-                size: '2048'
-            }), true)
-            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
-            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
-            .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Presence', message.author.presence) //The presence of this user
-            .addField('Presence Status', message.author.presence.status) //The presence status of this user
-            .addField('Presence Game', message.author.presence.activity.name) //The presence Game of this user
-            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
-            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
-            .setTimestamp() //The timestamp of this embed
-        
-        message.channel.send(KullanıcıBilgi);
-    }
-}); */
 
-//YARDIM
-/*
-client.on('message', msg => {
-    if (msg.content === prefix + 'yardım') {
-    const Yardım = new Discord.RichEmbed()
-      .setColor("RED")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .addField("Komutlarımız Şunlardır:", `${prefix}yardım = Komutları gösterir`, `${prefix}ping = Gecikmenizi gösterir`, `${prefix}yaz <mesajınız> = Bota mesajınızı yazdırırsınız`, `${prefix}sil = Sohbeti siler`, `${prefix}sunucu = Sunucu bilgisini gösterir`, `${prefix}emojiler = Bulunduğun sunucudaki emojileri gösterir`,)
-      .setDescription(`${client.user.username} Komut Yardımı`)
-      .setFooter("Zappara | Yardım")
-      .setTimestamp()
-    const YardımSunucu = new Discord.RichEmbed()
-      .setColor("GREEN")
-      .setDescription("Özel mesajlarını kontrol et :postbox:")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .setFooter("Zappara | Yardım")
-      .setTimestamp()
-    msg.channel.send(YardımSunucu).then(msg.author.send(Yardım));
-  }
-}); */
-
+//KOMUTLAR
 client.on('message', msg => {
   if (msg.content.toLowerCase() === prefix + 'komutlar') {
     if (msg.channel.type !== 'dm') {
