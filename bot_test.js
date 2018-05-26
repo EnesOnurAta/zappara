d42ee87570687332cb3328068203337a55d2be42
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-27 00:45:44 +0300

    Update bot_test.js

--- bot_test.js
+++ bot_test.js
@@ -17,3 +17,73 @@ client.on("message", async msg => {
     commandFile.run(client, msg, args);
   } catch (err) {}
 });
+
+// BOT AÇILINCA
+client.on('ready', () => {
+  console.log('Giris Saglandi');
+  console.log("Prefix: " + prefix);
+  console.log("Bot ID'si:" + client.user.id);
+  console.log("Bot Isim:" + client.user.username);
+});
+
+// OYNUYOR
+client.on('ready', () => {
+  client.user.setStatus("STREAMING"); 
+  client.user.setActivity('-komutlar | Zappara | -davet', {
+    type: "STREAMING"
+  }); 
+})
+//SELAM
+client.on('message', async msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    await msg.react('🇦');
+    msg.react('🇸');
+  }
+});
+// Otomatik Mesajlar
+client.on('message', msg => {
+  if (msg.content === 'Zappara') {
+   	msg.reply('Efendim canım');
+  }
+});
+// SUNUCUYA GİRİŞ
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let joinRole = guild.roles.find('name', 'Üye'); // Burada girişte verilcek rolu seçelim.
+  member.addRole(joinRole); // seçtiğimiz rolu verelim.
+
+  const channel = member.guild.channels.find('name', 'zappara'); // burda ise kanalı belirleyelim hangi kanala atsın ben giris-log dedim.
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📥 | Sunucuya katıldı! | Hoşgeldin!')
+  .setTimestamp()
+  channel.sendEmbed(embed); // belirlediğimiz kanala mesaj gönderelim.
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'zappara');/* cikis-log isimli kanalınız varsa log oraya gidecektir*/
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📤 | Sunucudan ayrıldı | Görüşmek üzere!')
+  .setTimestamp()
+  channel.sendEmbed(embed); 
+});
+// Özelden Yazılanlar
+    client.on("message", message => {
+    const dmchannel = client.channels.find("name", "zappara-dm");
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        dmchannel.sendMessage("", {embed: {
+                color: 3447003,
+                title: `DM'den Yazan: ${message.author.tag}`,
+                description: `${message.content}`
+              }})
+    }
+    if (message.channel.bot) return;
+});
