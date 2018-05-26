800666405fdea082cc3c1928f32a68efd4392741
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-27 00:37:47 +0300

    Create komutlar.js

---
+++ ozellikler/komutlar.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+exports.run('message', msg => {
+  if (msg.content.toLowerCase() === prefix + 'komutlar') {
+    if (msg.channel.type !== 'dm') {
+      const KOMUTLAR = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .addField(msg.author.username, 'Özel mesajlarını kontrol et. :postbox:');
+    msg.channel.sendEmbed(KOMUTLAR) }
+      msg.author.sendMessage('`Komutlar:\n\n' + prefix + 'komutlar\n' + prefix + 'ping\n' + prefix + 'davet\n' + prefix + 'destek\n`').then(message => console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Gönderilen mesaj: ${message.content}`)).catch(console.error);
+  }
+});
