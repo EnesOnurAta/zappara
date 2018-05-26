2ed0c4315a8b3d15b80244339cc3bfde71aa8946
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-27 00:18:43 +0300

    v1
    Yeni bir sisteme geçtik

---
+++ bot_test.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+
+var prefix = "-";
+
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if(msg.content.indexOf(prefix) !== 0) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const event = msg.content.toLower
+
+  try {
+    let commandFile = require(`./ozellikler/${command}.js`);
+    commandFile.run(client, msg, args);
+  } catch (err) {}
+});
