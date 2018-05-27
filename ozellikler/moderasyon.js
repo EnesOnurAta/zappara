53f3a1d4810cab4e70d80610b1da178259b2f13b
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-28 00:43:18 +0300

    Moderasyon
    Yasak Kaldırma (unban) eklendi

--- ozellikler/moderasyon.js
+++ ozellikler/moderasyon.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 let embed = new Discord.RichEmbed()
     .setTitle("<:TAmod:444855093569781760> Moderasyon")
     .addField("`" + message.prefix + "yasakla`", "Kullanıcıyı yasaklar")
-    .addField("`" + message.prefix + "yasaklama`", "Kullanıcının yasağını kaldırır [IDyi kullanın]")
+    .addField("`" + message.prefix + "yasak-kaldır`", "Kullanıcının yasağını kaldırır [IDyi kullanın]")
     .addField("`" + message.prefix +"sil`", "Mesajları siler")
     .addField("`" + message.prefix +"at`", "Kullanıcıyı atar")
     .addField("`" + message.prefix +"uyar`", "Kullanıcıyı uyarırsınız")
