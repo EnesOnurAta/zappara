d4b1eb6004ec7f9d8aed2c1903be0d4ddaf8353d
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-27 21:39:21 +0300

    Hatalar
    Oynuyor sadeleştirildi
    At temizlendi
    yasakla temizlendi
    
    minecraft yaratıkları gözden geçirildi

--- bot.js
+++ bot.js
@@ -33,10 +33,8 @@ client.on('ready', () => {
 // OYNUYOR
 client.on('ready', () => {
   client.user.setStatus("STREAMING"); 
-  client.user.setActivity('Teşekkürler: ${client.guilds.size} sunucu', {
-    type: "STREAMING"
-  }); 
-})
+  client.user.setActivity(`Teşekkürler: ${client.guilds.size} sunucu`); 
+});
 //SELAM
 client.on('message', async msg => {
   if (msg.content.toLowerCase() === 'sa') {
@@ -95,49 +93,32 @@ client.on('guildMemberRemove', member => {
 //MODERASYON KOMUTLARI - BAN,KICK,
 client.on("message", async message => {
   if(command === "at") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
     if(!message.member.roles.some(r=>["Zappara Team"].includes(r.name)) )
       return message.reply("Üzgünüm, bu komutu Zappara Team rolüne sahip olanlar kullanabilir!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
       return message.reply("Lütfen bu sunucuda olan bir üyeyi belirtin @üyeninadı#kodu şeklinde");
     if(!member.kickable) 
       return message.reply("Bu kullanıcıyı atamam! Daha yüksek bir rolleri var mı? Atma izinlerim var mı?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "Nedeni yok";
-    
-    // Now, time for a swift kick in the nuts!
     await member.kick(reason)
       .catch(error => message.reply(`Üzgünüm ${message.author} üyeyi atamadım : ${error}`));
     message.reply(`${member.user.tag} isimli üye ${message.author.tag} tarafından atıldı çünkü: ${reason}`);
 
   }
-  
+});  
 //BAN - Yasaklama
   if(command === "yasakla") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
     if(!message.member.roles.some(r=>["Zappara Team"].includes(r.name)) )
       return message.reply("Üzgünüm, bu komutu Zappara Team rolüne sahip olanlar kullanabilir!");
-    
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Lütfen bu sunucuda olan bir üyeyi belirtin @üyeninadı#kodu şeklinde");
     if(!member.bannable) 
       return message.reply("Bu kullanıcıyı yasaklayamam! Daha yüksek bir rolleri var mı? Yasaklama yetkim var mı?");
-
     let reason = args.slice(1).join(' ');
-    if(!reason) reason = "Nedeni yok";
-    
+    if(!reason) reason = "Nedeni yok";    
     await member.ban(reason)
       .catch(error => message.reply(`Üzgünüm ${message.author} , yasaklayamadım çünkü: ${error}`));
     message.reply(`${member.user.tag} isimli üye ${message.author.tag} tarafından yasaklandı çünkü: ${reason}`);
@@ -145,25 +126,14 @@ client.on("message", async message => {
 });
 //MINECRAFT MOB
 client.on("message", msg => {
-
   if (msg.content === prefix + 'mcyaratık') {
-
 const embed = new Discord.RichEmbed()
-
   .setTitle("Minecraft Yaratıkları")
-
   .setAuthor("Zappara", "https://cdn.discordapp.com/attachments/440820385643233290/449932544700579842/images_1.png")
-
   .setColor(0x00AE86)
-
   .setFooter("Zappara | Minecraft Yaratıkları", "https://cdn.discordapp.com/attachments/440820385643233290/449932544700579842/images_1.png")
-
   .setImage("https://cdn.discordapp.com/attachments/440820289312522261/445144265333538817/giphy.gif")
-
   .setTimestamp()
-
   .setURL("http://enesonurata.cf")
-
   msg.channel.send({embed})
-
   }});
