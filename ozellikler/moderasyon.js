e4c861de737d4eb78c88f4c66cf8068a13759dd6
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-28 01:23:46 +0300

    Moderasyon

--- ozellikler/moderasyon.js
+++ ozellikler/moderasyon.js
@@ -3,13 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let embed = new Discord.RichEmbed()
-    .setTitle("<:TAmod:444855093569781760> Moderasyon")
+    .setTitle(":tools: Moderasyon Komutları")
     .addField("`" + message.prefix + "yasakla`", "Kullanıcıyı yasaklar")
     .addField("`" + message.prefix + "yasak-kaldır`", "Kullanıcının yasağını kaldırır [IDyi kullanın]")
     .addField("`" + message.prefix +"sil`", "Mesajları siler")
     .addField("`" + message.prefix +"at`", "Kullanıcıyı atar")
     .addField("`" + message.prefix +"uyar`", "Kullanıcıyı uyarırsınız")
-    .setFooter("Mesajları zappara/mod-log kanalına atar!")
+    .addField("`" + message.prefix +"uyarıbak`", "Kullanıcının uyarılarına bakarsınız")
+    .setFooter("Gördüğünüz her şey JavaScript'tir")
 
 message.channel.send(embed);
 }
