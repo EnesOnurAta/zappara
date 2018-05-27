98659c6961a00e90f9c9f38b1fd16e2886e0cdf6
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-28 00:39:33 +0300

    YASAKLA
    mesajlar kalınlarştırıldı
    emoji hataları giderildi
    üper süper bişey oldu :D
    benden bu kadar

--- ozellikler/yasakla.js
+++ ozellikler/yasakla.js
@@ -3,20 +3,20 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(bUser === message.author) return message.channel.send("Özürlü müsün? Neden kendini yasaklamak istiyorsun?")
-    if(!bUser) return message.channel.send("Üye bulunamadı");
+    if(!bUser) return message.channel.send(":name_badge: **| Üye bulunamadı.** Lütfen @üye şeklinde etiketleyin yada ID sini girin");
     let bReason = args.join(" ").slice(22);
-   if(!bReason) return message.channel.send("Lütfen bir sebep belirtin!")
+   if(!bReason) return message.channel.send(":pencil2: **| Lütfen bir sebep belirtin!**")
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("<:tickNo:432418492667396097> **| Senin `BAN_MEMBERS` yetkin yok.**");
 
     let banEmbed = new Discord.RichEmbed()
-    .setDescription("Yasakla")
+    .setDescription("**Zappara | Yasakla**")
     .setColor("#bc0000")
     .addField("Kullanıcı", bUser.user.tag)
     .addField("Yasaklayan", message.author.tag)
     .addField("Sebep", bReason)
     .setTimestamp()
     let incidentchannel = message.guild.channels.find(`name`, "zappara_yasaklananlar");
-    if(!incidentchannel) return message.channel.send("<:tickNo:432418492667396097> **| Sunucuda `zappara_yasaklananlar` kanalı bulunamadı.**");
+    if(!incidentchannel) return message.channel.send(":name_badge: **| Sunucuda `zappara_yasaklananlar` kanalı bulunamadı.**");
     let embed = new Discord.RichEmbed()
     .setTitle("Yasakla")
     .addField("Yasaklandı", message.guild.name)
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Yasaklayan", message.author.tag)
     .addField("Sebep", bReason)
     bUser.send(embed);
-  message.channel.send("<:tickYes:432418492889694210> **| Üye başarıyla yasaklandı.**")
+  message.channel.send(":white_check_mark:  **| Üye başarıyla yasaklandı.**")
   bUser.ban(bReason)
   incidentchannel.send(banEmbed);
 }
