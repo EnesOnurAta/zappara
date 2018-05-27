c7ffcf98ee0fb9c3fc28724a04b704ac1d0d95e1
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-28 00:15:42 +0300

    EĞLENCE

---
+++ ozellikler/eğlence.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    let embed = new Discord.RichEmbed()
+    .setTitle("<:TAfun:444855091585875979> Eğlence")
+    .setTimestamp()
+    .addField("`" + message.prefix +"avatar`", "Kullanıcının Avatarını gösterir")
+    .addField("`" + message.prefix +"köpek`", "Rastgele köpek fotoğrafı atar")
+    .addField("`" + message.prefix +"kedi`", "Rastgele kedi fotoğrafı atar")
+    .addField("`" + message.prefix +"sayıtut`", "1-100 arası bir sayı tutar")
+    .addField("`" + message.prefix +"slot`", "Slot oynarsınız. 3 tane yan yana emoji getiren kazanır!")
+    message.channel.send(embed)
+}
+module.exports.help = {
+    name: "eğlence"
+}
