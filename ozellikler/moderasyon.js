b31b1ad3170db66283b24be579a14a5d2d294940
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-27 23:59:59 +0300

    Moderasyon
    yasakla
    yasaklama
    sil
    at
    uyar

---
+++ ozellikler/moderasyon.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let embed = new Discord.RichEmbed()
+    .setTitle("<:TAmod:444855093569781760> Moderasyon")
+    .addField("`" + message.prefix + "yasakla`", "Kullanıcıyı yasaklar")
+    .addField("`" + message.prefix + "yasaklama`", "Kullanıcının yasağını kaldırır [IDyi kullanın]")
+    .addField("`" + message.prefix +"sil`", "Mesajları siler")
+    .addField("`" + message.prefix +"at`", "Kullanıcıyı atar")
+    .addField("`" + message.prefix +"uyar`", "Kullanıcıyı uyarırsınız")
+    .setFooter("Mesajları zappara/mod-log kanalına atar!")
+
+message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "moderasyon"
+}
