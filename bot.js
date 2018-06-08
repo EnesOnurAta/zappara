cd1d54eac5a53b7f6b96310402fa864cc4437241
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-08 14:38:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,13 @@ client.on("guildDelete", guild => {
         ];
         let rstatus = Math.floor(Math.random() * status.length);
 		client.user.setStatus("STREAMING");
-	});
+		
+	client.user.setActivity(status[rstatus], {Type: 'STREAMING'});        // BOT STATUS
+      }; setInterval(botStatus, 20000)
+        setInterval(() => {
+        dbl.postStats(client.guilds.size)
+        }, 1800000);
+	})
 
 client.on('guildCreate', guild => {
     let channel = bot.channels.get("450419544519999509")
